0e2f9ddb4a1ba4ed5f5dbd608302e82f3d8fc418
Bruno da Cunha <brunodacunha@outlook.com>
2015-04-26 01:26:18 -0300

    Gerado método para carro correr

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,10 +8,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/app/example/}", name="homepage")
      */
-    public function indexAction()
+    public function indexAction($teste)
     {
         return $this->render('default/index.html.twig');
     }
+
+    /**
+     * @Route("/run/{marca}/{modelo}", name="run")
+     */
+    public function runAction($marca, $modelo){
+        return $this->render('AppBundle:Default:index.html.twig' , ['marca'=>strtoupper($marca), 'modelo'=>strtoupper($modelo)]);
+    }
+
 }
