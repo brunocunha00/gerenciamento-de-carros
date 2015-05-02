ed495acd85ebe403ed2ba2efe643fbed52fd3107
Bruno da Cunha <brunodacunha@outlook.com>
2015-05-02 17:24:06 -0300

    Criado controller CarController Criado método indexAction retornando marcas com seus respectivos modelos

---
+++ src/Code/CarBundle/Controller/CarController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Code\CarBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class CarController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array(
+                'carros' => array(
+                    ['marca' =>'chevrolet', 'modelo' => ['corsa', 'celta', 'prisma', 'camaro', 'blazer']],
+                    ['marca' =>'volkswagen', 'modelo' => ['golf', 'saveiro', 'fox', 'gol', 'polo']],
+                    ['marca' =>'renault', 'modelo' => ['duster', 'scenic', 'megane', 'laguna', 'sandero']]
+                )
+            );
+    }
+
+}
